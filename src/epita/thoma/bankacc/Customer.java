12bbb2bd7d4ed3962058b861fefa5e97d787abf0
devazyne <devzyne08@gmail.com>
2023-09-19 22:12:43 +0200

    first commit through intellij

---
+++ src/epita/thoma/bankacc/Customer.java
@@ -0,0 +1,20 @@
+package epita.thoma.bankacc;
+
+public class Customer {
+    private String name;
+    private String address;
+
+    public Customer(String address , String name)
+    {
+        this.address = address ;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
---
+++ src/epita/thoma/bankacc/lanucher/Main.java
@@ -0,0 +1,11 @@
+package epita.thoma.bankacc.lanucher;
+import epita.thoma.bankacc.Customer;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hey");
+        Customer customer = new Customer("aggg" , "deva");
+
+        System.out.println(customer);
+    }
+}
